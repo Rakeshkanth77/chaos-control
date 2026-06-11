606142113d18282c1bd0e0ce5af42316c545b941
rakeshkanth77 <rakeshkanth77@gmail.com>
2026-06-11 11:14:06 +0100

    Configure PostgreSQL parsing with dj-database-url for Railway production

--- productivityhub/settings.py
+++ productivityhub/settings.py
@@ -69,12 +69,21 @@ WSGI_APPLICATION = 'productivityhub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Use PostgreSQL in production if DATABASE_URL is set (e.g., in Railway)
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
